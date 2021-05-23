c1fa93586bef9024ea034377f866ce449a30b874
nobuyukiikeda <mail@nobuyuki-ikeda.com>
2021-05-23 18:51:15 +0900

    anyhow 桁数エラー追加

--- src/bin/err_anyhow.rs
+++ src/bin/err_anyhow.rs
@@ -1,9 +1,14 @@
-use anyhow::{Context, Result};
+use anyhow::{Context, Result, bail};
 
 fn get_int_from_file() -> Result<i32> {
   let path = "number.text";
   let num_str = std::fs::read_to_string(path)
     .with_context(|| format!("failed to read string from {}", path))?;
+  
+  if num_str.len() >= 10 {
+    bail!("It may be too large number");
+  }
+
   num_str.trim()
     .parse::<i32>()
     .map(|t| t * 2)
