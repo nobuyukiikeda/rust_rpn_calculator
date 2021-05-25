ff4caf5d67a5a80e2c99f3188429b2a27b9dd837
nobuyukiikeda <mail@nobuyuki-ikeda.com>
2021-05-25 21:19:18 +0900

    ロジック実装

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
-use std::{fs::File, io::{BufRead, BufReader, stdin}};
+use std::{fs::File, io::{BufRead, BufReader, stdin}, path::PathBuf};
 
+use anyhow::Result;
 use clap::Clap;
-use rpn_calculator::RpnCalculator;
 mod rpn_calculator;
 
 #[derive(Clap, Debug)]
@@ -19,44 +19,39 @@ struct Opts {
 
     // Formulas written in RPN
     #[clap(name = "FILE")]
-    formula_file: Option<String>,
+    formula_file: Option<PathBuf>,
 }
 
-fn main() {
+fn main() -> Result<()> {
     let opts = Opts::parse();
-
-    // match opts.formula_file {
-    //     Some(file) => println!("File specified: {}", file),
-    //     None => println!("No file specified"),
-    // }
-
-    // println!("Is verbosity specified?: {}", opts.verbose);
 
     if let Some(path) = opts.formula_file {
         let f = File::open(path).unwrap();
         let reader = BufReader::new(f);
-        run(reader, opts.verbose);
+        run(reader, opts.verbose)
     } else {
         let stdin = stdin();
         let reader = stdin.lock();
-        run(reader, opts.verbose);
+        run(reader, opts.verbose)
     }
 }
 
-fn run<R: BufRead>(reader: R, verbose: bool) {
-    let calc = RpnCalculator::new(verbose);
+fn run<R: BufRead>(reader: R, verbose: bool) -> Result<()> {
+    let calc = rpn_calculator::RpnCalculator::new(verbose);
 
     for line in reader.lines() {
-        let line = line.unwrap();
-        let answer = calc.eval(&line);
-        println!("{}", answer);
+        let line = line?;
+        match calc.eval(&line) {
+            Ok(answer) => println!("{}", answer),
+            Err(e) => eprintln!("{:#?}", e),
+        }
     }
+
+    Ok(())
 }
 
 #[cfg(test)]
 mod tests {
-    use super::*;
-
     #[test]
     fn test_ok() {
         assert_eq!(2 * 2, 4);
